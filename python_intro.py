e8f848c48b7b4d588047bb68fa5ce3bde89d1b4a
James_Muguro <kamandemuguro@gmail.com>
2023-10-31 10:15:20 +0300

    added comments to explain each section of the  code, included f-strings to provide more informative output,  and fixed the typo in 'MacOS' for the list element

--- python_intro.py
+++ python_intro.py
@@ -15,3 +15,24 @@ surnames = ("Muthoni", "Karmago", "Njuguna", "Omondi", "Kibet", "Kigen", "Mutiso
 # Iterate through the surnames and print each one
 for name in surnames:
     print(name)
+
+
+# Random Numbers
+import random
+
+# Generate a random integer between 1 and 10
+random_number = random.randint(1, 10)
+print(f"Random Integer: {random_number}")
+
+# Generate a random floating-point number between 0 and 1
+random_float = random.random()
+print(f"Random Float: {random_float}")
+
+# Generate a random element from a list
+my_list = ['Windows', 'MacOS', 'Android']
+random_element = random.choice(my_list)
+print(f"Random Element from List: {random_element}")
+
+# Shuffle the elements in a list
+random.shuffle(my_list)
+print(f"Shuffled List: {my_list}")
