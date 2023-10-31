f31411f00ba0eb9863bdd8011d17291eb9f0f3c3
James_Muguro <kamandemuguro@gmail.com>
2023-10-31 10:21:11 +0300

    added comments to describe what each part of the code does and used proper formatting for better readability. Additionally, I included the secret number in the winning message for clarity

--- python_intro.py
+++ python_intro.py
@@ -47,3 +47,25 @@ age = input("How old are you?: ")
 
 # Print a greeting with the user's name and age
 print(f"Hello, my name is {name}, and I am {age} years old")
+
+
+# Game
+import random
+
+# Generate a random secret number between 1 and 100
+secret_number = random.randint(1, 100)
+num_guesses = 0
+
+print("I'm thinking of a number between 1 and 100. Can you guess what it is?")
+
+while True:
+    guess = int(input("Enter your guess: "))
+    num_guesses += 1
+
+    if guess == secret_number:
+        print(f"Congratulations, you guessed the number {secret_number} in {num_guesses} guesses!")
+        break
+    elif guess < secret_number:
+        print("Too low! Try again.")
+    else:
+        print("Too high! Try again.")
